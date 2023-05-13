6d548cc72d4b043ef889c81314e264e6297635ef
Ákos Ferenc Kalamár <akos@Akoss-MacBook-Air.local>
2023-05-13 23:47:39 +0200

    playing the cards again :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import "./App.scss";
 import Card from "./components/card/Card";
 
 type cardItem = {
   name: string;
-}
+};
 
 const CARDS: cardItem[] = [
   { name: "Photo" },
@@ -18,21 +18,18 @@ const CARDS: cardItem[] = [
   { name: "Google translate" },
   { name: "Cocktail recipes" },
   { name: "Resume" },
-]
+];
 
 function App() {
   return (
     <div className="App">
       {/* Hej trevligt att träffas!  */}
-      
+
       {CARDS.map((item) => {
-        return (
-          <Card key={item.name} name={item.name}></Card>
-        )
+        return <Card key={item.name} name={item.name}></Card>;
       })}
     </div>
   );
-
 }
 
 export default App;
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,70 +1,65 @@
-import React, { ReactNode, useRef } from 'react';
+import React, { ReactNode, useRef } from "react";
 
-import './card.css';
-import { useState } from 'react';
+import "./card.scss";
+import { useState } from "react";
 
 interface CardProps {
-    name: string;
-    children?: ReactNode;
+  name: string;
+  children?: ReactNode;
 }
 
 type MouseEvent = React.MouseEvent<HTMLDivElement> | undefined;
 type DivElement = React.RefObject<HTMLDivElement>;
 
-
 const Card: React.FC<CardProps> = (props: CardProps) => {
-    const containerRef = useRef<HTMLDivElement>(null);
-    const [mouseX, setMouseX] = useState<number>(0);
-    const [mouseY, setMouseY] = useState<number>(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [mouseX, setMouseX] = useState<number>(0);
+  const [mouseY, setMouseY] = useState<number>(0);
 
-    function onMouseMoveHandler(event: MouseEvent): void {
-        rotateElement(event, containerRef)
-    }
+  function onMouseMoveHandler(event: MouseEvent): void {
+    rotateElement(event, containerRef);
+  }
 
-    function onMouseLeaveHandler(event: MouseEvent) {
-        containerRef.current?.style.setProperty("--rotateX", 0 + "deg");
-        containerRef.current?.style.setProperty("--rotateY", -1 * 0 + "deg");
-    }
+  function onMouseLeaveHandler(event: MouseEvent) {
+    containerRef.current?.style.setProperty("--rotateX", 0 + "deg");
+    containerRef.current?.style.setProperty("--rotateY", 0 + "deg");
+  }
 
-    function rotateElement(event: MouseEvent, element: DivElement) {
+  function rotateElement(event: MouseEvent, element: DivElement) {
+    // get mouse position
+    setMouseX(event!.nativeEvent.offsetX);
+    setMouseY(event!.nativeEvent.offsetY);
 
-        // get mouse position
-        setMouseX(event!.nativeEvent.offsetX);
-        setMouseY(event!.nativeEvent.offsetY);
+    // find the middle
+    const middleX = element.current!.clientWidth / 2; //window.innerWidth / 2;
+    const middleY = element.current!.clientHeight / 2; //window.innerHeight / 2;
+    // console.log(middleX, middleY)
 
-        //console.log(`mouse position: x:${x}, y:${y} } }`)
+    // get offset from middle as a percentage
+    // and tone it down a little
+    const offsetX = ((mouseX - middleX) / middleX) * 10;
+    const offsetY = ((mouseY - middleY) / middleY) * 10;
+    //console.log(offsetX, offsetY);
 
-        // find the middle
-        const middleX = element.current!.clientWidth / 2;  //window.innerWidth / 2;
-        const middleY = element.current!.clientHeight / 2; //window.innerHeight / 2;
-        // console.log(middleX, middleY)
+    // set rotation
+    element.current?.style.setProperty("--rotateX", offsetX + "deg");
+    element.current?.style.setProperty("--rotateY", -1 * offsetY + "deg");
+  }
 
-        // get offset from middle as a percentage
-        // and tone it down a little
-        const offsetX = ((mouseX - middleX) / middleX) * 10;
-        const offsetY = ((mouseY - middleY) / middleY) * 10;
-        // console.log(offsetX, offsetY);
-
-        // set rotation
-        element.current?.style.setProperty("--rotateX", offsetX + "deg");
-        element.current?.style.setProperty("--rotateY", -1 * offsetY + "deg");
-    }
-
-    return (
-        <div className='card-container'
-            onMouseMove={onMouseMoveHandler}
-            onMouseLeave={onMouseLeaveHandler}>
-            <div
-                ref={containerRef}
-                className='card-container-front'>
-                
-                {`mouse x: ${mouseX}`}
-                {`mouse y: ${mouseY}`}
-                {props.name}
-                {props.children}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div
+      className="card-container"
+      onMouseMove={onMouseMoveHandler}
+      onMouseLeave={onMouseLeaveHandler}
+    >
+      <div ref={containerRef} className="card-container-front">
+        {/* {`mouse x: ${mouseX}`}
+        {`mouse y: ${mouseY}`} */}
+        {props.name}
+        {props.children}
+      </div>
+    </div>
+  );
+};
 
 export default Card;
