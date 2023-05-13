ad54fd1c0ec52e0d2d5bd51b90583cb1376b85df
Ákos Ferenc Kalamár <afk@Akoss-Mac-mini.local>
2023-05-13 21:20:00 +0200

    play with cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from "react";
 import "./App.css";
+import Card from "./components/card/Card";
+
+type cardItem = {
+  name: string;
+}
+
+const CARDS: cardItem[] = [
+  { name: "Photo" },
+  { name: "Weather" },
+  { name: "Rick and Morty" },
+  { name: "Crypto" },
+  { name: "Domain Search" },
+  { name: "Chuck Norris" },
+  { name: "Pokemon" },
+  { name: "NASA" },
+  { name: "Citibikes" },
+  { name: "Google translate" },
+  { name: "Cocktail recipes" },
+  { name: "Resume" },
+]
 
 function App() {
-  return <div className="App">Hej trevligt att träffas!</div>;
+  return (
+    <div className="App">
+      {/* Hej trevligt att träffas!  */}
+      
+      {CARDS.map((item) => {
+        return (
+          <Card key={item.name} name={item.name}></Card>
+        )
+      })}
+    </div>
+  );
+
 }
 
 export default App;
